43431f047cec81770807ea8f696a5153817bcb94
ian-IBCIRL <ian@ibcirl.com>
2022-12-01 17:53:53 +0000

    initial commit with some new experiments on arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,51 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+x = 53455;
+y = 43545;
+
+let sum = addTwoNumbers(x,y);
+console.log("the result of adding ",x ," and ", y ," is ", sum);
 
 // Arrow Function With Parameters
+const  addTwoNumberz = (a, b) => {
+    a = a + b;
+    return a + b;
+}
+
+x = 5345;
+y = 4355;
+
+sum = addTwoNumberz(x,y);
+console.log("the result of adding ",x ," and ", y ,"with an arrow function is ", sum);
 
 // Single Line Arrow Function With Parameters
+// Arrow Function With Parameters
+const  addTwoNumberzOneLine = (a, b) => a + b + b;
+
+x = 534;
+y = 435;
+
+sum = addTwoNumberzOneLine(x,y);
+console.log("the result of adding ",x ," and ", y ,"with one line is ", sum);
+
 
 // Implicit Returns
 
 // Returning Multiple Lines
+// Arrow Function With Parameters
+const  addTwoNumberztext = (a, b) => (
+`<p> ` + a +
+    ` This is some text in a multiline string ` + b +
+` </p>`)
+
+
+let p="we";
+let q="you";
+
+result = addTwoNumberztext("we","you");
+console.log("the result of the function for",p ," and ", q ,"with one line is ", result); 
